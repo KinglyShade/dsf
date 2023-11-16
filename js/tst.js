659d8667ca25ec63e421267fb1813d2ccd5bbb34
Kingly Shade <150642678+KinglyShade@users.noreply.github.com>
2023-11-15 22:30:37 -0700

    fix bugs

--- js/tst.js
+++ js/tst.js
@@ -144,12 +144,12 @@ class PLB {
         if (pn) {
             return pn;
         }else{
-                await this.guardarEnBaseDeDatos(texto, 'No tengo respuesta en este momento.');
+                await this.guardarEnBaseDeDatos(`${texto}`, 'No tengo respuesta en este momento.');
            const fs = Math.floor(Math.random() * 2 + 1)
-           if(fs ==="1"){
+           if(fs =="1"){
             return `${this.experr[Math.floor(Math.random() * this.experr.length + 1)]}. ${this.expresionesTimidas[Math.floor(Math.random() * this.expresionesTimidas.length)]}`;        }
            
-           if(fs ==="2"){
+           if(fs =="2"){
             return `${this.cnf[Math.floor(Math.random() * this.cnf.length + 1)]}. ${this.expresionesTimidas[Math.floor(Math.random() * this.expresionesTimidas.length)]}`;        }
            }
     }
